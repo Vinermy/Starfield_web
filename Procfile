4726e393f3d16022a792a9aba98fdbe1accce3b7
Vinermy <egorkosachev@vk.com>
2023-06-29 17:50:15 +0700

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'Starfield_web.wsgi'
---
+++ starfield_web_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StarfieldWebAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'starfield_web_app'
