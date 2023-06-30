2f0601979471cf7076d07f7260df026af5cf72cd
Vinermy <egorkosachev@vk.com>
2023-06-30 21:52:44 +0700

    Implemented logging in, added an empty profile page

--- starfield_web_app/urls.py
+++ starfield_web_app/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
-    path('auth/signup', signup, name='auth-signup')
+    path('auth/signup', signup, name='auth-signup'),
+    path('auth/login', log_in, name='auth-login'),
+    path('auth/logout', logout_view, name='auth-logout'),
+    path('profile', profile_page, name='profile-page')
 ]
--- starfield_web_app/views.py
+++ starfield_web_app/views.py
@@ -1,4 +1,6 @@
 from django.contrib import messages, auth
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.forms import BaseForm
 from django.http import HttpRequest
@@ -20,14 +22,43 @@ def dashboard(request: HttpRequest):
 
 def signup(request: HttpRequest):
     if request.method == 'POST':
-        form = SignupForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
 
-        if not form.is_valid():
-            form_errors_as_messages(request, form)
-            return redirect('auth-signup')
+        user = User.objects.create_user(username=username, password=password)
 
-        user = form.save()
         auth.login(request, user)
         return redirect('dashboard')
 
     return render(request, 'auth/signup.html')
+
+
+def log_in(request: HttpRequest):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, password)
+
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('profile-page')
+        else:
+            messages.warning(request, 'Неверный логин и/или пароль')
+            return redirect('auth-login')
+
+    return render(request, 'auth/login.html')
+
+
+def logout_view(request: HttpRequest):
+    logout(request)
+    return redirect('auth-login')
+
+
+@login_required
+def profile_page(request: HttpRequest):
+    context = {
+        'user': request.user
+    }
+    return render(request, 'profile-page.html', context)
